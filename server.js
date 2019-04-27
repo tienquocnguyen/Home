3f66ae7ee15dc8c496ee7232acf771f528df7cf4
Tien Nguyen <tienquocnguyen22@gmail.com>
2019-04-26 22:57:32 -0700

    rez is now changed to res

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 
-app.get('/', (req,res) => resizeBy.send('Hello'));
+app.get('/', (req,res) => res.send('Hello'));
 
 app.listen(port, ()=> console.log('Server running on port ${port}'));
