b62204f42aca32bfd02407c626c2cd39ea0c4485
Tien Nguyen <tienquocnguyen22@gmail.com>
2019-04-26 22:53:46 -0700

    Server-side Setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+app.get('/', (req,res) => resizeBy.send('Hello'));
+
+app.listen(port, ()=> console.log('Server running on port ${port}'));
