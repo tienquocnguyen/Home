0b1d42d342ec9eb70c19900ab21faa1b348a14c8
Unknown <zjeffreyl@gmail.com>
2019-04-27 16:14:30 -0700

    Connect to Stripe

--- front_end/src/containers/donate.js
+++ front_end/src/containers/donate.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import DonatedNumber from './donatedNumber';
 import BankStatements from '../components/BankStatements'
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import CheckoutForm from '../components/CheckoutForm';
 
 export class donate extends Component {
   
   state = {
+    totalDonated: 0,
     bankStatements : [
     {
-      charge: 21.35
+      charge: 21.35,
     },
     {
       charge: 34.40
@@ -15,17 +18,31 @@ export class donate extends Component {
     {
       charge: 5.24
     }
-  ]
+    ]
+  }
+
+  calculateRound = (charge) => {
+    return Math.round(charge)
   }
 
   render() {
     return (
+        <StripeProvider apiKey="pk_test_VghJyHJS1r7oCxjuWxZZmcak00G848caUQ">
+        <div className="example">
+          <h1>React Stripe Elements Example</h1>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+
         <div>
-            <h1>Donations</h1>
+            <h1 >Donations</h1>
             <DonatedNumber />
             <BankStatements bankStatements={this.state.bankStatements}/>
             <p>This is the donations page</p>
+            <button onclick="donate()">Click me</button>
         </div>
+        </StripeProvider>
     )
   }
 }
